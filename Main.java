bc0c148dd09c5b686c2cd1e10596607f5df2799c
Araav Nayak <104335810+AraavNayak@users.noreply.github.com>
2022-08-06 15:37:50 -0700

    Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+//Araav Nayak
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner s;
+	
+	public static void main(String[] args) {
+		s = new Scanner(System.in);
+		getFactors(s.nextInt());
+	  s.close();
+  }
+  
+}
